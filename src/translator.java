7df16a9ab7c0f1b001dc83846db39519ae39e7f0
Jinsung Sunwoo <jinsung.sunwoo@gmail.com>
2016-09-14 17:41:30 +0900

    Finsh first sentence

--- src/translator.java
+++ src/translator.java
@@ -7,37 +7,61 @@ import java.util.StringTokenizer;
  */
 
 public class translator {
+    private static StringBuffer fullSentence = new StringBuffer("#include <stdio.h>\n" +
+            "int main()\n{\n\t");
+
     public static void main(String[] args) {
+
         try {
             BufferedReader in;
-            StringTokenizer st;
-            String readLine;
+            String readL;
 
             in = new BufferedReader(new FileReader("test.txt"));
 
-            while ((readLine = in.readLine()) != null) {
-                System.out.println(readLine);
-                StringTokenizer firstToken = new StringTokenizer(readLine, " ");
-                tokenCheck(firstToken);
+            while ((readL = in.readLine()) != null) {
+                System.out.println(readL);
+                tokenCheck(readL);
             }
+            System.out.println(fullSentence);
 
             in.close();
-
-
-            String token = "";
-            while (st.hasMoreElements()) {
-
-                token = st.nextToken();
-
-
-                System.out.println(token.charAt(0));
-
-            }
         } catch (Exception e) {
             System.out.println("Exception error");
         }
     }
 
-    private static void tokenCheck(StringTokenizer firstToken) {
+    private static void tokenCheck(String readLine) {
+
+        StringTokenizer st = new StringTokenizer(readLine, " ");
+        String firstToken = st.nextToken();
+        String val1 = "";
+        String val2 = "";
+        int numbefOfthings = 0;
+
+
+        if (firstToken.equals("(def")) {
+            fullSentence.append("long ");
+            String nextToken = "";
+            nextToken = st.nextToken(); // 첫번째 변수이름 읽어서
+            val1 = nextToken;       // 저장하고
+            fullSentence.append(nextToken);     // 출력
+            fullSentence.append("[] = ");     // 출력
+
+            while (st.hasMoreElements()) {
+                nextToken = st.nextToken();
+                if (nextToken.startsWith("[")) {
+                    String replacedToken = nextToken.replace('[', '{');
+                    fullSentence.append(replacedToken);
+                } else if (nextToken.endsWith(")")) {
+                    String replacedToken = nextToken.replace("])", "}");
+                    fullSentence.append(replacedToken);
+                } else {
+                    fullSentence.append(nextToken);
+                }
+                numbefOfthings++;
+            }
+            fullSentence.append(";\n");
+            fullSentence.append("\tint _AD_size = " + numbefOfthings + ";\n");
+        }
     }
 }
