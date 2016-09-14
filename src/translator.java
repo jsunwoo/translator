001c57b248cbf3e6d966627e08ba5089668fa843
Jinsung Sunwoo <jinsung.sunwoo@gmail.com>
2016-09-14 10:38:07 +0900

    Make translator`

---
+++ src/translator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jinsu on 2016-09-14.
+ */
+public class translator {
+    public static void main(String[] args) {
+        System.out.print("test");
+        System.out.println("test" + "hi1");
+    }
+}
