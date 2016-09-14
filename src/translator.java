3284265474685abae13664b326b1d08e57d5f4b4
Jinsung Sunwoo <jinsung.sunwoo@gmail.com>
2016-09-14 11:25:36 +0900

    Make tokenizer

--- src/translator.java
+++ src/translator.java
@@ -1,9 +1,21 @@
+import java.util.StringTokenizer;
+
 /**
  * Created by jinsu on 2016-09-14.
  */
 public class translator {
     public static void main(String[] args) {
-        System.out.print("test");
-        System.out.println("test" + "hi1");
+        String temp = "대한민국 미국 영국";
+
+        StringTokenizer st = new StringTokenizer(temp," ");
+
+        String token = "";
+        while (st.hasMoreElements()) {
+
+            token = st.nextToken();
+
+            System.out.println(token);
+        }
     }
+
 }
