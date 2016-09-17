7ac0cedfb280d7dc96fc984859233b38f246528a
Jinsung Sunwoo <jinsung.sunwoo@gmail.com>
2016-09-17 12:13:22 +0900

    Make second row

--- src/translator.java
+++ src/translator.java
@@ -34,8 +34,8 @@ public class translator {
 
         StringTokenizer st = new StringTokenizer(readLine, " ");
         String firstToken = st.nextToken();
-        String val1 = "";
-        String val2 = "";
+        String id1;
+//        String val2 = "";
         int numbefOfthings = 0;
 
 
@@ -43,15 +43,15 @@ public class translator {
             fullSentence.append("long ");
             String nextToken = "";
             nextToken = st.nextToken(); // 첫번째 변수이름 읽어서
-            val1 = nextToken;       // 저장하고
+            id1 = nextToken;       // 저장하고
             fullSentence.append(nextToken);     // 출력
             fullSentence.append("[] = ");     // 출력
 
             while (st.hasMoreElements()) {
-                nextToken = st.nextToken();
-                if (nextToken.startsWith("[")) {
+                nextToken = st.nextToken();     // 토큰읽고
+                if (nextToken.startsWith("[")) {        // 바꿔준다음에
                     String replacedToken = nextToken.replace('[', '{');
-                    fullSentence.append(replacedToken);
+                    fullSentence.append(replacedToken);     // 붙이기
                 } else if (nextToken.endsWith(")")) {
                     String replacedToken = nextToken.replace("])", "}");
                     fullSentence.append(replacedToken);
@@ -62,6 +62,21 @@ public class translator {
             }
             fullSentence.append(";\n");
             fullSentence.append("\tint _AD_size = " + numbefOfthings + ";\n");
+        } else if (firstToken.equals("(reduce")) {
+            String id2;
+            String op;
+            String initialVal;
+
+            id2 = st.nextToken(); // id2 먼저 저장하고
+            op = st.nextToken();    // 계산식 구분하고
+            initialVal = st.nextToken();
+
+            fullSentence.append("\tint _AD_i = 0;\n");
+            fullSentence.append("\tint _AD_result = "+ initialVal + ";\n\n");
+            fullSentence.append("\tfor(_AD_i = 0; _AD_i < _AD_size; _AD_i++ {\n");
+            fullSentence.append("\t\t_AD_result "+ op + "= " + id2 + "[_AD_i];\n");
+            fullSentence.append("\t}\n");
         }
+
     }
 }
